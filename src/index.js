eb0362d9fe67a23714bf22440d2c99074661e757
unknown <samir1maity@gmail.com>
2024-03-16 02:15:14 +0530

    Added details to readme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const {PORT} = require("./config/serverConfig")
+const { PORT } = require("./config/serverConfig");
 
 const bodyParser = require("body-parser");
 
 const ServerFlightSearch = () => {
 
-   // create the express server
-   const app = express();
+  // create the express server
+  const app = express();
+  
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-   app.listen(PORT, () => {
-     console.log(`Server started at ${PORT}`);
-   });
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+  });
 
-}
+};
 
 ServerFlightSearch();
